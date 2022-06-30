af856389fa17d8315357275bd4477b9ea00766cf
unknown <abdulwahabiddris08@gmail.com>
2022-06-30 17:14:34 +0100

    i

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import '../styles/fun.css'
-
+import Head from "next/head"
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+<div>
+<style>
+ @import url({"https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"});
+ @import url({"https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"});
+ @import url({"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"});
+ @import url({"https://cdn.lineicons.com/3.0/lineicons.css"});
+</style>
+<Component {...pageProps} />
+</div>
+)
 }
 
 export default MyApp
